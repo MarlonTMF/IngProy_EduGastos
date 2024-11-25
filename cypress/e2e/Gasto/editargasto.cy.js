4925ac4670eb26ac6fcd505e0f5d2f8f1665d27d
Mar14n423 <wara.mercado@ucb.edu.bo>
2024-11-25 04:48:43 -0400

    No se permite editar el gasto sino estan llenos los campos de fecha y monto

--- cypress/e2e/Gasto/editargasto.cy.js
+++ cypress/e2e/Gasto/editargasto.cy.js
@@ -21,30 +21,49 @@ describe("Botón Editar junto a cada gasto", () => {
     });
   
     it("Debe cargar los datos del gasto en el formulario al presionar Editar", () => {
-      // Presionar el botón Editar
+     
       cy.get("#gastos-div .gasto-item").contains("Editar").click();
   
-      // Verificar que los campos del formulario tienen los valores correctos
       cy.get("#fecha").should("have.value", "2024-11-01");
       cy.get("#monto").should("have.value", "100");
       cy.get("#descripcion").should("have.value", "Cine");
     });
     it("Debe permitir modificar un gasto existente", () => {
-        // Presionar el botón Editar
         cy.get("#gastos-div .gasto-item").contains("Editar").click();
     
-        // Modificar los valores en el formulario
         cy.get("#fecha").clear().type("2024-11-05");
         cy.get("#monto").clear().type("150");
         cy.get("#descripcion").clear().type("Teatro");
     
-        // Registrar los cambios
         cy.get("#registrar-gasto-button").click();
     
-        // Verificar que los datos actualizados aparecen en el listado de gastos
         cy.get("#gastos-div").should("contain", "2024-11-05")
                              .and("contain", "150")
                              .and("contain", "Teatro");
+    });
+    it("no debería editar un gasto si falta fecha o monto", () => {
+        cy.get("#fecha").type("2024-11-01");
+        cy.get("#monto").type("100");
+        cy.get("#descripcion").type("Cine");
+        cy.get("#registrar-gasto-button").click();
+    
+        cy.get("#gastos-div").should("contain", "Cine");
+    
+        cy.get(".editar-gasto").first().click(); 
+        cy.get("#fecha").clear(); 
+        cy.get("#monto").clear().type("150"); 
+        cy.get("#descripcion").clear().type("Teatro"); 
+        cy.get("#registrar-gasto-button").click(); 
+    
+        cy.get("#gastos-div").should("contain", "Cine");
+        cy.get("#gastos-div").should("not.contain", "Teatro");
+    
+        cy.get(".editar-gasto").first().click(); 
+        cy.get("#fecha").type("2024-11-05"); 
+        cy.get("#monto").clear().type("150"); 
+        cy.get("#descripcion").clear().type("Teatro"); 
+        cy.get("#registrar-gasto-button").click(); 
+        cy.get("#gastos-div").should("contain", "Teatro");
     });
 
   });
